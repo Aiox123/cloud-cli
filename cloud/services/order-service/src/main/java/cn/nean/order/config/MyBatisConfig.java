bc9a6983a040347b07b9928cfc2be5f197a985b4
nean <2801912027@qq.com>
2023-02-05 17:23:15 +0800

    MyBatis first Fix

---
+++ cloud/services/order-service/src/main/java/cn/nean/order/config/MyBatisConfig.java
@@ -0,0 +1,13 @@
+package cn.nean.order.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author nean
+ */
+@MapperScan("cn.nean.mapper")
+@Configuration
+public class MyBatisConfig {
+
+}
