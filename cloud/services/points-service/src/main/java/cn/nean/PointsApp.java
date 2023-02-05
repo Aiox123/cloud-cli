9b706e9f07fce6d5304bcab5ef5f3593e2f2271c
nean <2801912027@qq.com>
2023-02-05 17:00:02 +0800

    CloudAlibaba_Cli

---
+++ cloud/services/points-service/src/main/java/cn/nean/PointsApp.java
@@ -0,0 +1,11 @@
+package cn.nean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointsApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PointsApp.class,args);
+    }
+}
